fe5367ab7a67dbbfc01008b4d68f4ab9a02c64d3
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2025-06-09 00:17:52 -0300

    :test_tube: test: added mainly tests for link and url

--- src/test/java/com/shortener/link/domain/entities/LinkTest.java
+++ src/test/java/com/shortener/link/domain/entities/LinkTest.java
@@ -1,8 +1,25 @@
 package com.shortener.link.domain.entities;
 
 
+import com.shortener.link.domain.value_objects.Url;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class Link {
+public class LinkTest {
+    @Test
+    public void testCreateLinkCorrectly() {
+        Link link = new Link(new Url("https://mock.com"), new Url("https://mock.com"), "short");
+        Assertions.assertEquals(new Url("https://mock.com"), link.originalUrl);
+        Assertions.assertEquals(new Url("https://mock.com/short"), link.getShortUrl());
+    }
+
+    @Test
+    public void testGenerateShortLinkCorrectly() {
+        Link link = Link.shorten(new Url("https://mock.com/originalUrl"), new Url("https://mock.com"));
+        Assertions.assertNotEquals(link.originalUrl, link.getShortUrl());
+        Assertions.assertTrue(link.getShortUrl().toString().startsWith("https://mock.com/"));
+        Assertions.assertTrue(link.getShortUrl().toString().length() < link.originalUrl.toString().length());
+    }
 }
