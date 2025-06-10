b23f524a6e8774683544bbbc2ba3133ce20cf548
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2025-06-09 21:14:49 -0300

    :sparkles: feat: added expired fn on link

--- src/main/java/com/shortener/link/domain/entities/Link.java
+++ src/main/java/com/shortener/link/domain/entities/Link.java
@@ -3,19 +3,39 @@ package com.shortener.link.domain.entities;
 import com.shortener.link.domain.value_objects.Url;
 
 import java.security.MessageDigest;
+import java.util.Date;
 
 public class Link {
     public final Url originalUrl;
     private final String shortHash;
     private final Url baseUrl;
+    private final Integer duration;
+    private final Date createdDate;
 
-    public Link(Url originalUrl, Url baseUrl, String shortHash) {
+    private static final int MINUTE_IN_MILLISECONDS = 60 * 1000;
+    private static final int THREE_DAYS_IN_MILLISECONDS = 3 * 24 * 60 * 60 * 1000;
+
+    public Link(Url originalUrl, Url baseUrl, String shortHash, Date createdDate, Integer duration) {
         this.originalUrl = originalUrl;
         this.baseUrl = baseUrl;
         this.shortHash = shortHash;
+        this.createdDate = createdDate;
+
+        if (duration == null) {
+            this.duration = null;
+            return;
+        }
+
+        if (duration <  MINUTE_IN_MILLISECONDS || duration > THREE_DAYS_IN_MILLISECONDS)
+            throw new IllegalArgumentException("A duração deve estar entre 1 minuto e 3 dias");
+        this.duration = duration;
     }
 
-    public static Link shorten(Url originalUrl, Url baseUrl) {
+    public Link(Url originalUrl, Url baseUrl, String shortHash, Date createdDate) {
+        this(originalUrl, baseUrl, shortHash, createdDate, null);
+    }
+
+    public static Link shorten(Url originalUrl, Url baseUrl, Integer duration) {
         try {
             MessageDigest messageDigest = MessageDigest.getInstance("MD5");
             byte[] urlBytes = messageDigest.digest(originalUrl.toString().getBytes());
@@ -24,13 +44,23 @@ public class Link {
             for (byte urlByte : urlBytes) hexString.append(String.format("%02x", urlByte));
 
             String hash = hexString.substring(0, 8);
-            return new Link(originalUrl, baseUrl, hash);
+            return new Link(originalUrl, baseUrl, hash, new Date(), duration);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new IllegalArgumentException(e);
         }
+    }
+
+    public static Link shorten(Url originalUrl, Url baseUrl) {
+        return shorten(originalUrl, baseUrl, null);
     }
 
     public Url getShortUrl() {
         return new Url(baseUrl.toString() + "/" + shortHash);
+    }
+
+    public boolean isExpired() {
+        if (duration == null) return false;
+        Date expirationDate = new Date(createdDate.getTime() + duration);
+        return expirationDate.before(new Date());
     }
 }
--- src/test/java/com/shortener/link/domain/entities/LinkTest.java
+++ src/test/java/com/shortener/link/domain/entities/LinkTest.java
@@ -6,20 +6,54 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+
 @SpringBootTest
 public class LinkTest {
     @Test
     public void testCreateLinkCorrectly() {
-        Link link = new Link(new Url("https://mock.com"), new Url("https://mock.com"), "short");
+        Link link = new Link(new Url("https://mock.com"), new Url("https://mock.com"), "short", new Date());
         Assertions.assertEquals(new Url("https://mock.com"), link.originalUrl);
         Assertions.assertEquals(new Url("https://mock.com/short"), link.getShortUrl());
     }
 
     @Test
     public void testGenerateShortLinkCorrectly() {
-        Link link = Link.shorten(new Url("https://mock.com/originalUrl"), new Url("https://mock.com"));
+        Link link = Link.shorten(new Url("https://mock.com/originalUrl"), new Url("https://mock.com"), 60 * 60 * 1000);
         Assertions.assertNotEquals(link.originalUrl, link.getShortUrl());
         Assertions.assertTrue(link.getShortUrl().toString().startsWith("https://mock.com/"));
         Assertions.assertTrue(link.getShortUrl().toString().length() < link.originalUrl.toString().length());
+    }
+
+    @Test
+    public void testGenerateShortLinkInvalidDuration() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Link.shorten(new Url("https://mock.com"), new Url("https://mock.com"), 0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Link.shorten(new Url("https://mock.com"), new Url("https://mock.com"), 10 * 24 * 60 * 60 * 1000));
+    }
+
+    @Test
+    public void testIsNotExpired() {
+        int duration = 3 * 24 * 60 * 60 * 1000;
+        Link link = Link.shorten(new Url("https://mock.com"), new Url("https://mock.com"), duration);        Assertions.assertFalse(link.isExpired());
+    }
+
+
+    @Test
+    public void testIsExpired() {
+        int duration = 3 * 24 * 60 * 60 * 1000;
+        Link link = new Link(new Url("https://mock.com"), new Url("https://mock.com"), "short", new Date(new Date().getTime() - duration - 1), duration);
+        Assertions.assertTrue(link.isExpired());
+    }
+
+    @Test
+    public void testIsNotExpiredWhenDurationIsNull() {
+        Link link = Link.shorten(new Url("https://mock.com"), new Url("https://mock.com"));
+        Assertions.assertFalse(link.isExpired());
+    }
+
+    @Test
+    public void testGetShortUrl() {
+        Link link = Link.shorten(new Url("https://mock.com"), new Url("https://mock.com"));
+        Assertions.assertTrue(link.getShortUrl().toString().startsWith("https://mock.com/"));
     }
 }
