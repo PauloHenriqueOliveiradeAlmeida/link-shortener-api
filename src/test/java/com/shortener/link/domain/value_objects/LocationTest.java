3aad3ab7925444d0346a2c03d697cc2dca7b156e
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2025-06-14 21:21:45 -0300

    :test_tube: test: added location tests

--- src/test/java/com/shortener/link/domain/value_objects/LocationTest.java
+++ src/test/java/com/shortener/link/domain/value_objects/LocationTest.java
@@ -1,4 +1,24 @@
 package com.shortener.link.domain.value_objects;
 
+import com.shortener.link.domain.enums.Uf;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class LocationTest {
+
+    @Test
+    public void testCreateLocationCorrectly() {
+        Location location = new Location("Cidade", Uf.AC);
+        Assertions.assertEquals("Cidade", location.city());
+        Assertions.assertEquals(Uf.AC, location.uf());
+    }
+
+    @Test
+    public void testCreateLocationInvalid() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Location(null, Uf.AC));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Location("", Uf.AC));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Location("Cidade", null));
+    }
 }
