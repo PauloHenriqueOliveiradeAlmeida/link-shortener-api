ce68595fbf259186dd3b438ddd62ee3c97da8664
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2025-06-23 20:26:52 -0300

    :checkmark: test: added container decorator

--- src/test/java/com/shortener/link/infrastructure/config/LocalStackConfig.java
+++ src/test/java/com/shortener/link/infrastructure/config/LocalStackConfig.java
@@ -3,6 +3,7 @@ package com.shortener.link.infrastructure.config;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
@@ -11,6 +12,7 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 @TestConfiguration(proxyBeanMethods = false)
 public class LocalStackConfig {
 
+    @Container
     private static final DockerImageName LOCALSTACK_IMAGE_NAME = DockerImageName.parse("localstack/localstack:latest");
 
     @Bean
