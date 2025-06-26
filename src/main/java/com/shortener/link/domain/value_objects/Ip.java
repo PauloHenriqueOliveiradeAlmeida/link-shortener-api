2e0e48c6b7e51aa53a1234051f0ec0a144c15ec9
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2025-06-26 10:27:44 -0300

    :recycle: refactor: changed ip record to use inetaddress class

--- src/main/java/com/shortener/link/domain/value_objects/Ip.java
+++ src/main/java/com/shortener/link/domain/value_objects/Ip.java
@@ -1,13 +1,23 @@
 package com.shortener.link.domain.value_objects;
 
+import java.net.InetAddress;
+
 public record Ip(String value, Location location) {
     public Ip {
-        if (value == null) throw new IllegalArgumentException("IP deve ser informado");
-        if (value.isEmpty()) throw new IllegalArgumentException("IP nao pode ser vazio");
-        if (!value.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}"))
-            throw new IllegalArgumentException("IP inválido");
-        if (value.equals("0.0.0.0")) throw new IllegalArgumentException("IP não pode ser 0.0.0.0");
-        if (value.equals("127.0.0.1")) throw new IllegalArgumentException("IP não pode ser 127.0.0.1");
-        if (location == null) throw new IllegalArgumentException("Localidade deve ser informada");
+        if (value.isBlank()) throw new IllegalArgumentException("IP deve ser informado");
+        if (location == null) throw new IllegalArgumentException("Localização deve ser informada");
+
+        try {
+            InetAddress inetAddress = InetAddress.getByName(value);
+            boolean isInvalidIp = inetAddress.isLoopbackAddress() ||
+                    inetAddress.isLinkLocalAddress() ||
+                    inetAddress.isSiteLocalAddress() ||
+                    inetAddress.isMulticastAddress() ||
+                    inetAddress.isAnyLocalAddress();
+            if (isInvalidIp) throw new IllegalArgumentException("IP inválido");
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException("IP inválido");
+        }
     }
 }
